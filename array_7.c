3c31ef7a935451266b074ac0e30e2e3ca4ccf63a
Krishangi verma <krishangiverma52@gmail.com>
2026-02-03 15:09:24 +0530

    now short an array in ascending order

---
+++ array_7.c
@@ -0,0 +1,34 @@
+// now short an array in ascending order
+#include<stdio.h>
+void sortdescending(int arr[],int n){
+    int temp;
+    for(int i=0;i<n-1;i++){
+    for(int j=i+1;j<n;j++){
+if(arr[i]>arr[j]){
+    temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("enter number of elements: ");
+    scanf("%d",&n);
+
+    int arr[n];
+    printf("enter %d elements: \n",n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+}
+sortdescending(arr,n);
+
+printf("array in ascending order: \n");
+for(int i=0;i<n;i++){
+    printf("%d\n",arr[i]);
+}
+printf("\n");
+return 0;
+}
