3bbcfdc43eeaa97a155025b617a317d87ae6fe6c
Krishangi verma <krishangiverma52@gmail.com>
2026-02-03 15:11:25 +0530

    Added a c program that add two matrices

---
+++ add_matrix.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[3][3], b[3][3], c[3][3];
+    int i, j;
+
+    printf("Enter elements of first 3x3 matrix:\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("a[%d][%d] = ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nEnter elements of second 3x3 matrix:\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("b[%d][%d] = ", i, j);
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Adding matrices
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("\nResultant matrix after addition:\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
